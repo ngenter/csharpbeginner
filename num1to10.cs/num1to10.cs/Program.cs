403dd647f85d0300dfaacac366f3b087e3d87099
ngenter <njg401@yahoo.com>
2016-08-23 04:22:58 -0400

    Added num1to10.cs

---
+++ num1to10.cs/num1to10.cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace num1to10.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number between 1 and 10: ");
+            var num = Convert.ToInt32(Console.ReadLine());
+
+            if (num >= 1 && num <= 10)
+                Console.WriteLine("That number is valid");
+            else
+                Console.WriteLine("That number is invalid");
+        }
+    }
+}
