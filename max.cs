9845f4f82e70b0c8f4da8d2189c01e7b93d15b20
ngenter <njg401@yahoo.com>
2016-08-23 04:16:08 -0400

    Changed code to looker cleaner

--- max.cs
+++ max.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.Write("Please enter a number: ");
+            Console.WriteLine("Please enter a number: ");
             var input1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter another number: ");
             var input2 = Convert.ToInt32(Console.ReadLine());
